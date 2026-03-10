e8c8aa57061dfdd0725f109bdc45a3dd01971ea3
yoanadn <yoananedelcheva_zh21@schoolmath.eu>
2026-03-10 07:58:14 +0200

    Updated

--- PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0390233f4dc383fca8e9277c937dcd06671ca03")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
